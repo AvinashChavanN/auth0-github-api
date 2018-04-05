4d39e59e54325878ba7a46597ee6c726142443d1
Avinash <chvn.avi@gmail.com>
2018-04-05 00:15:56 -0500

    Implemented Github API

---
+++ src/Component/Header.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {Navbar,Nav,NavItem} from 'react-bootstrap';
+
+export default class Header extends Component {
+  onLogin(){
+    this.props.onLogin();
+  }
+  render(){
+    return(
+      <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              Github Searcher
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem onClick={this.onLogin.bind(this)} >Login
+            </NavItem>
+          </Nav>
+      </Navbar>
+    );
+  }
+}
